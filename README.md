31f88bd8c78f82573e302b2cc0fdc3b3d61ce5e6
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2024-03-29 10:24:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rdf surfaces tests
+
+A test kit for [RDF Surfaces](https://w3c-cg.github.io/rdfsurfaces/).
---
+++ make_examples.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+RED="\033[31m"
+PINK="\033[35m"
+GREEN="\033[32m"
+NORMAL="\033[0;39m"
+
+OK=0
+FAILED=0
+SKIPPED=0
+
+if [ "$1" == "clean" ]; then
+    rm test/*.out 2> /dev/null
+    exit 0
+fi 
+
+for n3s in test/*.n3s ; do 
+    if [[ "$n3s" =~ SKIP ]] ; then
+        echo "(skipping $n3s)" | tee $n3s.out
+    else
+        echo "eye --nope --quiet $n3s > $n3s.out 2> /dev/null"
+        timeout 5s eye --nope --quiet $n3s > $n3s.out 2> /dev/null
+    fi
+done
+
+for f in $(find . -name "*.out" | sort) ; do
+    if [[ $f =~ examples ]]; then 
+        continue
+    fi
+
+    echo -n "Testing $f ... "
+
+    if [[ $f =~ FAIL ]] && [[ ! -s $f ]]; then
+        echo -e "${GREEN}OK${NORMAL}"
+        ((OK++))
+    elif [[ $f =~ SKIP ]]; then
+        echo -e "${PINK}SKIPPED${NORMAL}"
+        ((SKIPPED++))
+    elif [[ $(grep '.*:test.*is.*true' $f) ]]; then
+        echo -e "${GREEN}OK${NORMAL}"
+        ((OK++))
+    elif [[ $(grep '() log:onAnswerSurface true' $f) ]]; then
+        echo "OK"
+        ((OK++))
+    else
+        echo -e "${RED}FAILED${NORMAL}"
+        ((FAILED++))
+    fi
+done
+
+echo -e "Results: ${GREEN}${OK} OK${NORMAL}, ${RED}${FAILED} FAILED${NORMAL}, ${PINK}${SKIPPED} SKIPPED${NORMAL}"
+
+if [[ ${FAILED} -eq 0 ]]; then 
+    exit 0
+else    
+    exit 2
+fi
